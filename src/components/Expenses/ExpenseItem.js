2442cba7fdb501baacf3ecfc6bfe2f0b3c3cea4e
@glaeserl <logan.glaeser@wwt.com>
2022-02-01 17:43:08 -0600

    did the thing

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,7 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card.js";
 import React from "react";
+import { unstable_renderSubtreeIntoContainer } from "react-dom/cjs/react-dom.development";
 
 function ExpenseItem(props) {
   return (
@@ -11,6 +12,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={() => console.log("clcicked")}>Change Title</button>
     </Card>
   );
 }
